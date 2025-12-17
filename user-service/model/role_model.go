4ba77f60501ded1c4f881c91f7a4ee256a0e9bff
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-17 10:35:41 +0700

    add role and user models

--- user-service/model/role_model.go
+++ user-service/model/role_model.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Role struct {
-	ID  		uint   `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name 		string `json:"name"`
-	CreatedAt 	time.Time `json:"created_at"`
-	UpdatedAt 	time.Time `json:"updated_at"`
+	ID  		uint   		`json:"id" gorm:"primaryKey;autoIncrement"`
+	Name 		string 		`json:"name"`
+	CreatedAt 	time.Time 	`json:"created_at"`
+	UpdatedAt 	time.Time 	`json:"updated_at"`
 }
--- user-service/model/user_model.go
+++ user-service/model/user_model.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type User struct {
-	ID 			uint `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name 		string `json:"name"`
-	Email 		string `json:"email" gorm:"unique"`
-	Password 	string `json:"-"`
-	Photo 		string `json:"photo"`
-	Phone 		string `json:"phone"`
-	Roles 		[]Role `gorm:"many2many:user_role;"`
-	CreatedAt 	time.Time `json:"created_at"`
-	UpdatedAt 	time.Time `json:"updated_at"`
+	ID 			uint 		`json:"id" gorm:"primaryKey;autoIncrement"`
+	Name 		string 		`json:"name"`
+	Email 		string 		`json:"email" gorm:"unique"`
+	Password 	string 		`json:"-"`
+	Photo 		string 		`json:"photo"`
+	Phone 		string 		`json:"phone"`
+	Roles 		[]Role 		`gorm:"many2many:user_role;"`
+	CreatedAt 	time.Time 	`json:"created_at"`
+	UpdatedAt 	time.Time 	`json:"updated_at"`
 }
