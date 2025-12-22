a5b5d1350971b876cb82b6f94de70aecd5ee382d
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-22 17:30:06 +0700

    format

--- user-service/model/user_role_model.go
+++ user-service/model/user_role_model.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type UserRole struct {
-	ID    		uint   		`json:"id" gorm:"primaryKey;autoIncrement"`
-	UserID 		uint   		`json:"user_id" gorm:"not null"`
-	RoleID 		uint   		`json:"role_id" gorm:"not null"`
-	User		User	 	`gorm:"foreignKey:UserID"`
-	Role		Role	 	`gorm:"foreignKey:RoleID"`
-	CreatedAt 	time.Time  	`json:"created_at"`
-	UpdatedAt 	time.Time  	`json:"updated_at"`
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	UserID    uint      `json:"user_id" gorm:"not null"`
+	RoleID    uint      `json:"role_id" gorm:"not null"`
+	User      User      `gorm:"foreignKey:UserID"`
+	Role      Role      `gorm:"foreignKey:RoleID"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type Tabler interface {
