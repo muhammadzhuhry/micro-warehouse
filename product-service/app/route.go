d582702149b45abb321f9d3eaa91c30fb2c6fa53
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-24 01:37:03 +0700

    add route setup for categories and products

---
+++ product-service/app/route.go
@@ -0,0 +1,21 @@
+package app
+
+import "github.com/gofiber/fiber/v2"
+
+func SetupRoutes(app *fiber.App, container *Container) {
+	api := app.Group("/api/v1")
+
+	categories := api.Group("/categories")
+	categories.Post("/", container.CategoryController.CreateCategory)
+	categories.Get("/", container.CategoryController.GetAllCategories)
+	categories.Get("/:id", container.CategoryController.GetCategoryByID)
+	categories.Put("/:id", container.CategoryController.UpdateCategory)
+	categories.Delete("/:id", container.CategoryController.DeleteCategory)
+
+	products := api.Group("/products")
+	products.Post("/", container.ProductController.CreateProduct)
+	products.Get("/", container.ProductController.GetAllProducts)
+	products.Get("/:id", container.ProductController.GetProductByID)
+	products.Put("/:id", container.ProductController.UpdateProduct)
+	products.Delete("/:id", container.ProductController.DeleteProduct)
+}
