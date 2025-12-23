881f6cd9dfb572fa104b341cc1511898b3883817
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-23 20:20:37 +0700

    add category model

---
+++ product-service/model/category_model.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type Category struct {
+	ID        string     `json:"id" gorm:"primaryKey"`
+	Name      string     `json:"name" gorm:"type:varchar(100);not null"`
+	Tagline   string     `json:"tagline" gorm:"type:varchar(100);uniqueIndex"`
+	Photo     string     `json:"photo" gorm:"type:text"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	Products  []Product  `json:"products" gorm:"foreignKey:CategoryID"`
+}
