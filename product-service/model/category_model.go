36085b79b7493ee69f48e5c281e395243915dcec
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-24 00:23:33 +0700

    add category model

--- product-service/model/category_model.go
+++ product-service/model/category_model.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type Category struct {
-	ID        string     `json:"id" gorm:"primaryKey"`
+	ID        uint       `json:"id" gorm:"primaryKey"`
 	Name      string     `json:"name" gorm:"type:varchar(100);not null"`
 	Tagline   string     `json:"tagline" gorm:"type:varchar(100);uniqueIndex"`
 	Photo     string     `json:"photo" gorm:"type:text"`
