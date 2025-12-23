6f19046a38c3384e68a156c3798f72966e1bc029
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-24 00:23:39 +0700

    add product model

--- product-service/model/product_model.go
+++ product-service/model/product_model.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type Product struct {
-	ID         string     `json:"id" gorm:"primaryKey"`
+	ID         uint       `json:"id" gorm:"primaryKey"`
 	Name       string     `json:"name" gorm:"type:varchar(100);not null"`
 	Barcode    string     `json:"barcode" gorm:"type:varchar(100);uniqueIndex"`
 	CategoryID string     `json:"category_id"`
@@ -13,5 +13,6 @@ type Product struct {
 	IsPopular  bool       `json:"is_popular" gorm:"default:false"`
 	CreatedAt  time.Time  `json:"created_at"`
 	UpdatedAt  *time.Time `json:"updated_at"`
+	DeletedAt  *time.Time `json:"deleted_at"`
 	Category   Category   `json:"category,omitempty" gorm:"foreignKey:CategoryID"`
 }
