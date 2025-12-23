12392a1d9e99ac40798d7742adae77c61ff6887f
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-23 11:45:50 +0700

    fix many2many relation

--- user-service/model/role_model.go
+++ user-service/model/role_model.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Role struct {
-	ID  		uint   		`json:"id" gorm:"primaryKey;autoIncrement"`
-	Name 		string 		`json:"name"`
-	CreatedAt 	time.Time 	`json:"created_at"`
-	UpdatedAt 	time.Time 	`json:"updated_at"`
-	Users 		[]User		`json:"users" gorm:"foreignKey:RoleID"`
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Users     []User    `json:"users" gorm:"many2many:user_role;"`
 }
