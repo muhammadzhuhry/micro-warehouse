f85fcece9d7885ad622d996f70e33ff12790c208
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-23 11:48:18 +0700

    add auth controller to container

--- user-service/app/container.go
+++ user-service/app/container.go
@@ -14,6 +14,7 @@ import (
 type Container struct {
 	RoleController controller.RoleControllerInterface
 	UserController controller.UserControllerInterface
+	AuthController controller.AuthControllerInterface
 }
 
 func BuildContainer() *Container {
@@ -36,8 +37,11 @@ func BuildContainer() *Container {
 	userUsecase := usecase.NewUserUsecase(userRepo, rabbitMQService)
 	userController := controller.NewUserController(userUsecase)
 
+	authController := controller.NewAuthController(userUsecase)
+
 	return &Container{
 		RoleController: roleController,
 		UserController: userController,
+		AuthController: authController,
 	}
 }
