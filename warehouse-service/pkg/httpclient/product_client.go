9de31a382ca49e3fee97f3ec8ec81ba2c0404a61
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-01-02 21:19:41 +0700

    fix response struct

--- warehouse-service/pkg/httpclient/product_client.go
+++ warehouse-service/pkg/httpclient/product_client.go
@@ -27,6 +27,7 @@ type ProductResponse struct {
 	Name      string `json:"name"`
 	About     string `json:"about"`
 	Price     int64  `json:"price"`
+	Barcode   string `json:"barcode"`
 	Thumbnail string `json:"thumbnail"`
 	Category  struct {
 		ID    uint   `json:"id"`
