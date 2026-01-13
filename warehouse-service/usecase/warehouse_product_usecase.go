ae5cb629db2c19b7c8ee05acb0e245fd98c41eaf
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-01-13 16:34:18 +0700

    fix not found error

--- warehouse-service/usecase/warehouse_product_usecase.go
+++ warehouse-service/usecase/warehouse_product_usecase.go
@@ -7,6 +7,7 @@ import (
 	"micro-warehouse/warehouse-service/repository"
 
 	"github.com/gofiber/fiber/v2/log"
+	"gorm.io/gorm"
 )
 
 type WarehouseProductUsecaseInterface interface {
@@ -37,8 +38,10 @@ func NewWarehouseProductUsecase(warehouseProductRepo repository.WarehouseProduct
 func (w *warehouseProductUsecase) CreateWarehouseProduct(ctx context.Context, warehouseProduct *model.WarehouseProduct) error {
 	result, err := w.warehouseProductRepo.GetWarehouseProductByWarehouseIDAndProductID(ctx, warehouseProduct.WarehouseID, warehouseProduct.ProductID)
 	if err != nil {
-		log.Errorf("[WarehouseProductUsecase] CreateWarehouseProduct - 1: %v", err)
-		return err
+		if err != gorm.ErrRecordNotFound {
+			log.Errorf("[WarehouseProductUsecase] CreateWarehouseProduct - 1: %v", err)
+			return err
+		}
 	}
 
 	if result != nil {
