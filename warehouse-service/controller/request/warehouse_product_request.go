e613d1cc4426d722232a30b13fdb4af5a2d2154b
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-01-13 16:34:05 +0700

    fix warehouse id

--- warehouse-service/controller/request/warehouse_product_request.go
+++ warehouse-service/controller/request/warehouse_product_request.go
@@ -1,7 +1,6 @@
 package request
 
 type CreateWarehouseProductRequest struct {
-	WarehouseID uint `json:"warehouse_id" validate:"required"`
-	ProductID   uint `json:"product_id" validate:"required"`
-	Stock       int  `json:"stock" validate:"required"`
+	ProductID uint `json:"product_id" validate:"required"`
+	Stock     int  `json:"stock" validate:"required"`
 }
