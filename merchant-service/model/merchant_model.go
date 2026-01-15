11679f8b5b43e3f0bb2d685ddf28a9605a624af9
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-01-16 06:11:43 +0700

    feat: update Merchant and MerchantProduct models to use uint for ID fields

--- merchant-service/model/merchant_model.go
+++ merchant-service/model/merchant_model.go
@@ -3,15 +3,15 @@ package model
 import "time"
 
 type Merchant struct {
-	ID        int64      `json:"id" gorm:"primaryKey;autoIncrement"`
+	ID        uint       `json:"id" gorm:"primaryKey"`
 	Name      string     `json:"name" gorm:"type:varchar(100);not null"`
-	Address   string     `json:"address"`
+	Address   string     `json:"address" gorm:"type:text"`
 	Photo     string     `json:"photo"`
 	Phone     string     `json:"phone"`
-	KeeperID  uint64     `json:"keeper_id" gorm:"not null"`
+	KeeperID  uint       `json:"keeper_id" gorm:"not null"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty" gorm:"index"`
 
-	MerchantProducts []MerchantProduct `json:"merchant_products,omitempty" gorm:"foreignKey:MerchantID;references:ID"`
+	MerchantProducts []MerchantProduct `json:"merchant_products" gorm:"foreignKey:MerchantID"`
 }
--- merchant-service/model/merchant_product_model.go
+++ merchant-service/model/merchant_product_model.go
@@ -4,9 +4,9 @@ import "time"
 
 type MerchantProduct struct {
 	ID          uint       `json:"id" gorm:"primaryKey;autoIncrement"`
-	MerchantID  int64      `json:"merchant_id" gorm:"not null;index"`
-	ProductID   int64      `json:"product_id" gorm:"not null;index"`
-	WarehouseID int64      `json:"warehouse_id" gorm:"not null;index"`
+	MerchantID  uint       `json:"merchant_id" gorm:"not null;index"`
+	ProductID   uint       `json:"product_id" gorm:"not null;index"`
+	WarehouseID uint       `json:"warehouse_id" gorm:"not null;index"`
 	Stock       int        `json:"stock" gorm:"not null;default:0"`
 	CreatedAt   time.Time  `json:"created_at"`
 	UpdatedAt   *time.Time `json:"updated_at"`
