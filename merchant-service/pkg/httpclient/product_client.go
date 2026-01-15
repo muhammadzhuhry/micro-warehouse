2f988c1fe27ff88731a93c6724c987383f06d66a
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-01-15 19:01:04 +0700

    feat: implement GetProductByBarcode method in ProductClient

--- merchant-service/pkg/httpclient/product_client.go
+++ merchant-service/pkg/httpclient/product_client.go
@@ -15,6 +15,7 @@ import (
 
 type ProductClientInterface interface {
 	GetProductByID(ctx context.Context, productID uint) (*ProductResponse, error)
+	GetProductByBarcode(ctx context.Context, barcode string) (*ProductResponse, error)
 	GetProducts(ctx context.Context, page, limit int, search, sortBy, sortOrder string) ([]ProductResponse, error)
 	HealthCheck(ctx context.Context) error
 }
@@ -98,6 +99,44 @@ func (p *ProductClient) GetProductByID(ctx context.Context, productID uint) (*Pr
 	productResponse := ProductServiceResponse{}
 	if err := json.Unmarshal(body, &productResponse); err != nil {
 		log.Errorf("[ProductClient] GetProductByID - 5: %v", err)
+		return nil, err
+	}
+
+	return &productResponse.Data, nil
+}
+
+// GetProductByBarcode implements [ProductClientInterface].
+func (p *ProductClient) GetProductByBarcode(ctx context.Context, barcode string) (*ProductResponse, error) {
+	url := fmt.Sprintf("%s/api/v1/products/barcode/%s", p.urlProductService, barcode)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		log.Errorf("[ProductClient] GetProductByBarcode - 1: %v", err)
+		return nil, err
+	}
+
+	resp, err := p.httpClient.Do(req)
+	if err != nil {
+		log.Errorf("[ProductClient] GetProductByBarcode - 2: %v", err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("[ProductClient] GetProductByBarcode - 3: %v", err)
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Errorf("[ProductClient] GetProductByBarcode - 4: %s", string(body))
+		return nil, errors.New("failed to get product by barcode")
+	}
+
+	productResponse := ProductServiceResponse{}
+	if err := json.Unmarshal(body, &productResponse); err != nil {
+		log.Errorf("[ProductClient] GetProductByBarcode - 5: %v", err)
 		return nil, err
 	}
 
